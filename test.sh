646bbba3409819a7fa9f5678add9144be645cd1f
Kenna Wang <kenna.wang6@gmail.com>
2019-03-04 15:39:56 -0800

    test script

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
+do
+    curl -s http://web.cs.ucla.edu/classes/cs111/Samples/P3B-test_$i.csv -o test$i.csv
+    echo === RUNNING TEST $i ===
+    echo test result:
+    ./lab3b test$i.csv
+    echo provided result:
+    curl -s http://web.cs.ucla.edu/classes/cs111/Samples/P3B-test_$i.err
+    rm -f test$i.csv
+    echo
+done
